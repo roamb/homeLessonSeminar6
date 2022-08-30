d313e747576ba2ee8e9963be70e7dd2e89185d09
roan b <roman7tati@gmail.com>
2022-08-30 18:40:29 +0300

    first commit

---
+++ 1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите несколько чисел больше либо меньше нуля, используя пробел ");
+string input = Console.ReadLine();
+string[] inNum = input.Split(' ');
+int sum = 0;
+for (int i = 0; i < inNum.Length; i++)
+{
+    int num = Int32.Parse(inNum[i]);
+    if (num > 0) sum++;
+}
+Console.WriteLine("Чисел больше 0: " + sum);
---
+++ 2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine();
+Console.Write("Введите b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+Console.WriteLine($"Координаты ({x};{y})");
